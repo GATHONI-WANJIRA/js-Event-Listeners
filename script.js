c86c91de6bcc8b09de1df0229d6d67458bd9cd11
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-06-21 10:37:28 +0300

    AddEventListener

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello')
+const grandparent = document.querySelector(".grandparent")
+const parent = document.querySelector(".parent")
+const child = document.querySelector(".child")
+
+
+grandparent.addEventListener('click',e => {
+    console.log(e.target);
+})

c8dbc5c084a8da72abf4565ea0f92e759b208f6b
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-06-21 13:25:27 +0300

    Event Capturing

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 const grandparent = document.querySelector(".grandparent")
-// console.log(document);
-// const parent = document.querySelector(".parent")
-// const child = document.querySelector(".child")
-// console.log(grandparent);
+const parent = document.querySelector(".parent")
+const child = document.querySelector(".child")
 
-grandparent.addEventListener('click', e => {
-    console.log(e.target);
-})
+
+// Add eventListeners
+// grandparent.addEventListener('click', e => {
+//     console.log(e.target);
+// })
 
 // After adding the  eventListeners you need to add parameter. 
 // The parameter takes two arguments which is the event you want(eg click) and the callback function which runs every time you run your event... (in this case click) 
@@ -17,14 +17,51 @@ grandparent.addEventListener('click', e => {
 // It simply means going from the closest elements to the outer most element.
 // It happens when an element receives an event, and that event bubbles up (or you can say is transmitted or propagated) to its parent and ancestor elements in the DOM tree until it gets to the root element.
 
+// grandparent.addEventListener('click', e => {
+//     console.log(grandparent);
+// })
+
+// parent.addEventListener('click', e => {
+//     console.log(parent);
+// })
+
+// child.addEventListener('click', e => {
+//     console.log(child);
+// })
+
+// Event Capturing
+// Event capturing happens from the outside to the inside... (furthest to closest.)
+
+
 grandparent.addEventListener('click', e => {
-    console.log(grandparent);
+    console.log('grandparent capture');
+} ,{capture :true})
+
+grandparent.addEventListener('click', e => {
+    console.log('grandparent Bubble');
 })
+
 
 parent.addEventListener('click', e => {
-    console.log(parent);
-})
+    console.log('parent capture');
+}, {capture:true});
+
+parent.addEventListener('click', e => {
+    console.log('parent bubble');
+});
 
 child.addEventListener('click', e => {
-    console.log(child);
+    console.log('child capture');
+} , {capture: true})
+
+child.addEventListener('click', e => {
+    console.log('child bubble');
 })
+
+document.addEventListener('click', e => {
+    console.log('document bubble');
+})
+
+document.addEventListener('click', e => {
+    console.log('document capture');
+} , {capture: true})
